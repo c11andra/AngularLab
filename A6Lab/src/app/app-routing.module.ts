6c74da3b7fc6965cf1a9f6e1ded4be6d0370be9f
c11andra <c11andra@github.com>
2018-09-16 16:10:22 +0530

    Perform routing

--- A6Lab/src/app/app-routing.module.ts
+++ A6Lab/src/app/app-routing.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component'
+import {UsersComponent} from './users/users.component'
+import {PostsComponent} from './posts/posts.component'
+import {DetailsComponent} from './details/details.component'
 
 const routes: Routes = [
-  
+  {
+    path:'',
+    component:HomeComponent
+  },
+  {
+    path:'users',
+    component:UsersComponent
+  },
+  {
+    path: 'details/:id',
+    component: DetailsComponent
+  },
+  {
+    path:'posts',
+    component:PostsComponent
+  }
 ];
 
 @NgModule({
